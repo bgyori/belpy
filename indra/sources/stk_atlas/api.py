7486bb844ffbab579c1232ca8ab8d38a16af5bbe
Ben Gyori <ben.gyori@gmail.com>
2023-01-13 00:58:58 -0500

    Add more docstrings

--- indra/sources/stk_atlas/api.py
+++ indra/sources/stk_atlas/api.py
@@ -15,14 +15,34 @@ base_url = ('https://static-content.springer.com/esm/'
             'art%3A10.1038%2Fs41586-022-05575-3/MediaObjects/')
 
 
-# The maximum rank to consider for annotations to construct Statements
+# The maximum rank to consider for annotations to construct INDRA Statements
 DEFAULT_MAX_RANK = 15
 # The minimum percentile cutoff for annotations to construct INDRA Statements
 DEFAULT_MIN_PERCENTILE = 90
 
 
-def process_from_web(cache_local=False, max_rank=DEFAULT_MAX_RANK,
-                     min_percentile=DEFAULT_MIN_PERCENTILE):
+def process_from_web(cache_local: bool = False,
+                     max_rank: int = DEFAULT_MAX_RANK,
+                     min_percentile: float = DEFAULT_MIN_PERCENTILE) \
+        -> StkAtlasProcessor:
+    """Process the dataset from the web.
+
+    Parameters
+    ----------
+    cache_local: bool
+        If set to True, it loads the dataset from the local cache.
+    max_rank: int
+        The maximum rank to consider for annotations to construct INDRA
+        Statements.
+    min_percentile: float
+        The minimum percentile cutoff for annotations to construct INDRA
+        Statements.
+
+    Returns
+    -------
+    StkAtlasProcessor
+        A StkAtlasProcessor instance with the processed data.
+    """
     kinase_table_path = base_url + kinase_table
     annotation_table_path = base_url + annot_table
     # If we use caching, we override the URLs to be local paths
@@ -43,8 +63,31 @@ def process_from_web(cache_local=False, max_rank=DEFAULT_MAX_RANK,
                                   min_percentile=min_percentile)
 
 
-def process_local(kinase_xlsx, annot_csv, max_rank=DEFAULT_MAX_RANK,
-                  min_percentile=DEFAULT_MIN_PERCENTILE):
+def process_local(kinase_xlsx: str,
+                  annot_csv: str,
+                  max_rank: int = DEFAULT_MAX_RANK,
+                  min_percentile: float = DEFAULT_MIN_PERCENTILE):
+    """
+    Process the dataset from local files.
+
+    Parameters
+    ----------
+    kinase_xlsx : str
+        The path to the excel file containing the kinase data.
+    annot_csv : str
+        The path to the csv file containing the annotation data.
+    max_rank: int
+        The maximum rank to consider for annotations to construct INDRA
+        Statements.
+    min_percentile: float
+        The minimum percentile cutoff for annotations to construct INDRA
+        Statements.
+
+    Returns
+    -------
+    StkAtlasProcessor
+        A StkAtlasProcessor instance with the processed data.
+    """
     kinase_df = pd.read_excel(kinase_xlsx, sheet_name=kinase_sheet_name,
                               )
     annot_df = pd.read_csv(annot_csv)
@@ -52,8 +95,30 @@ def process_local(kinase_xlsx, annot_csv, max_rank=DEFAULT_MAX_RANK,
                                   min_percentile=min_percentile)
 
 
-def process_from_dataframe(kinase_df, annot_df, max_rank=DEFAULT_MAX_RANK,
-                           min_percentile=DEFAULT_MIN_PERCENTILE):
+def process_from_dataframe(kinase_df: pd.DataFrame,
+                           annot_df: pd.DataFrame,
+                           max_rank: int = DEFAULT_MAX_RANK,
+                           min_percentile: float = DEFAULT_MIN_PERCENTILE):
+    """Process the dataset from dataframes.
+
+    Parameters
+    ----------
+    kinase_df: pd.DataFrame
+        Dataframe containing kinase data.
+    annot_df: pd.DataFrame
+        Dataframe containing annotation data.
+    max_rank: int
+        The maximum rank to consider for annotations to construct INDRA
+        Statements.
+    min_percentile: float
+        The minimum percentile cutoff for annotations to construct INDRA
+        Statements.
+
+    Returns
+    -------
+    StkAtlasProcessor
+        A StkAtlasProcessor instance with the processed data.
+    """
     sp = StkAtlasProcessor(kinase_df, annot_df, max_rank=max_rank,
                            min_percentile=min_percentile)
     sp.process_phosphorylations()
